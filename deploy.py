f9cdfe38dec08d318424139b9f8ed6fdc5f22f6e
gggkio <121001335+gggkio@users.noreply.github.com>
2026-03-29 02:12:09 +0800

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -15,7 +15,7 @@ image = (
 def run_in_sandbox():
     print("🧪 Launching sandbox...")
 
-    sandbox = modal.Sandbox.create(app=app, image=image,timeout=86400,region="ap-northeast-3")
+    sandbox = modal.Sandbox.create(app=app, image=image,timeout=86400,region="ca-toronto-1")
 
     # ✅ 后台执行 app.py，不阻塞 GitHub Actions
     print("🚀 Running app.py in sandbox (background)...")
